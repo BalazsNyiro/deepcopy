073e72a44eb491a0c73f2e6d6585b7b97c4027e6
Balazs Nyiro <diogenesz@pergamen.hu>
2019-12-06 05:40:16 +0100

    Basic page layout

--- src/ui_tkinter.py
+++ src/ui_tkinter.py
@@ -20,19 +20,41 @@ def window_main(Prg):
     if Prg["Errors"]: return
 
     print("Tkinter ui main interface")
-    Window = window_new(Prg, "window.main.title")
+    MainWidth = 1200
+    MainHeight = 800
 
-    MainHeight = 600
-    FrameSourcePages = Tkinter.Frame(Window, bg='cyan', width=350, height=MainHeight, pady=3).grid(row=0, columnspan=3)
-    FrameOnePage = Tkinter.Frame(Window, bg='purple', width=800, height=MainHeight, pady=3).grid(row=0, columnspan=3)
-    FrameTextRecognised = Tkinter.Frame(Window)
+    Window = window_new(Prg, "window.main.title")
+    Window.geometry('{}x{}'.format(MainWidth, MainHeight))
+
+
+    SourceWidth = 300
+    OnePageWidth = 600
+    TextRecognisedWidth = MainWidth - SourceWidth - OnePageWidth
+    Pady = 3
+
+    FrameSourcePages = Tkinter.Frame(Window, bg='cyan', width=SourceWidth, height=MainHeight, pady=Pady)
+    FrameSourcePages.grid(row=0, column=0, rowspan=3)
+
+    FrameOnePage = Tkinter.Frame(Window, bg='purple', width=OnePageWidth, height=MainHeight, pady=Pady)
+    FrameOnePage.grid(row=0, column=1, rowspan=3)
+
+    FrameTextRecognised = Tkinter.Frame(Window, bg='green', width=TextRecognisedWidth, height=MainHeight,pady=Pady)
+    FrameTextRecognised.grid(row=0, column=2, rowspan=3)
 
     Img = image_file_load_to_tk(Prg, "resources/deepcopy_logo_64.png")
     if Img:
         Panel = Tkinter.Label(FrameSourcePages, image=Img)
-        Panel.grid(row=0)
-        Tkinter.Label(FrameOnePage, text="Frame One Page").grid(row=0)
-        Tkinter.Label(FrameTextRecognised, text="Text Recognised").pack()
+        Panel.grid(row=0, column=0)
+
+        Panel2 = Tkinter.Label(FrameSourcePages, image=Img)
+        Panel2.grid(row=1, column=1)
+
+        Panel3 = Tkinter.Label(FrameSourcePages, image=Img)
+        Panel3.grid(row=2, column=2)
+
+        Tkinter.Label(FrameOnePage, text="Frame One Page").grid(row=0, column=1)
+        Tkinter.Label(FrameOnePage, text="2222").grid(row=1, column=1)
+        Tkinter.Label(FrameTextRecognised, text="Text Recognised").grid(row=0, column=2)
 
     # top_frame = Frame(root, bg='cyan', width = 450, height=50, pady=3).grid(row=0, columnspan=3)
     # FrameSourcePages.grid(row=0, column=0, sticky='e')
