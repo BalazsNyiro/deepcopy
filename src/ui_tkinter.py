cde854135a61493292fd15efebb74f3a7ebf5e9d
Balazs Nyiro <diogenesz@pergamen.hu>
2019-12-07 19:16:41 +0100

    Load files to be recognised button

--- src/ui_tkinter.py
+++ src/ui_tkinter.py
@@ -15,27 +15,27 @@ try:    from PIL import ImageTk
 except ImportError: ErrorsLocal.append("install.missing.package_ImageTk")
 
 def window_main(Prg):
-    # store passed Prg as a global variable, too, because Tkinter buttons need a state
-    # I collect the msg NOT in the if because if one of them is missing, it causes Error
     for ErrTxtKey in ErrorsLocal:
         Prg["Errors"].append(util.ui_msg(Prg, ErrTxtKey))
 
     if Prg["Errors"]: return
 
+    # store passed Prg as a global variable, too, because Tkinter buttons need a state
+    # I collect the msg NOT in the if because if one of them is missing, it causes Error
     global PrgGlobal
     PrgGlobal = Prg
+
     Prg["Tkinter"] = {"images_loaded": {}}
 
-    print("Tkinter ui main interface")
     MainWidth = 1200
     MainHeight = 800
+    SourceWidth = 300
+    OnePageWidth = 600
 
     Window = window_new(Prg, "window.main.title")
     Window.geometry('{}x{}'.format(MainWidth, MainHeight))
     Prg["Tkinter"]["Window"] = Window
 
-    SourceWidth = 300
-    OnePageWidth = 600
     TextRecognisedWidth = MainWidth - SourceWidth - OnePageWidth
 
     FrameSourcePages = frame_new(Prg, Window, SourceWidth, MainHeight, bg="yellow")
@@ -50,39 +50,26 @@ def window_main(Prg):
     Tkinter.Label(FrameOnePage, text="Frame One Page").grid(row=0, column=1)
     Tkinter.Label(FrameOnePage, text="2222").grid(row=1, column=1)
 
-
     Prg["Tkinter"]["FrameSourcePages"] = FrameSourcePages
-    B = Tkinter.Button(FrameSourcePages, text="Hello", command=files_thumbnails_load_button)
-    B.pack()
-
+    Tkinter.Button(FrameSourcePages, text=util.ui_msg(Prg, "file_operation.file_load_into_thumbnail_list"), command=files_thumbnails_load_button).pack()
 
     Tkinter.Label(FrameTextRecognised, text="Text Recognised").grid(row=0, column=2)
 
-    # top_frame = Frame(root, bg='cyan', width = 450, height=50, pady=3).grid(row=0, columnspan=3)
-    # FrameSourcePages.grid(row=0, column=0, sticky='e')
-
     Window.mainloop()
 
-# FIXME: dynamic image inserting doesn't work correctly
-# https://python-forum.io/Thread-Tkinter-How-do-I-change-an-image-dynamically
-# ??
 def files_thumbnails_load_button():
     Prg = PrgGlobal
-    files_thumbnails_load(Prg, Prg["Tkinter"]["FrameSourcePages"], Prg["Tkinter"]["Window"])
-    print("button")
-def files_thumbnails_load(Prg, Parent, Window):
+    files_thumbnails_load(Prg, Prg["Tkinter"]["FrameSourcePages"])
+
+def files_thumbnails_load(Prg, Parent):
     for FileSelected in files_selector(Prg):
         ImgId = img_generate_id_for_loaded_list(Prg, "thumbnalis")
         Img = image_file_load_to_tk(Prg, FileSelected)
-        Img.ImgId = ImgId # all image knows his own id, if you want to remove them, delete them from loaded image list
-        print("img type: ", type(Img))
-
-        Prg["Tkinter"]["images_loaded"][ImgId] = Img # save reference of Img, otherwise garbace collector remove it
-
         if Img:
+            Img.ImgId = ImgId # all image knows his own id, if you want to remove them, delete them from loaded image list
+            Prg["Tkinter"]["images_loaded"][ImgId] = Img # save reference of Img, otherwise garbace collector remove it
             Panel = Tkinter.Label(Parent, image=Img)
             Panel.pack()
-            Window.update_idletasks()
 
 def files_selector(Prg):
     Dir = Prg["PathDefaultFileSelectDir"]
