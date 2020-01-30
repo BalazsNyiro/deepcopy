c484bca5fece730e0bdb3a17c91ac5600a8c1fec
Balazs Nyiro <diogenesz@pergamen.hu>
2020-01-30 20:47:12 +0100

    find blocks in mark, block generator

--- src/vector.py
+++ src/vector.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 
 def block_search_in_mark(Mark):
-    BlockWidth = min(Mark["Width"], Mark["Height"])
-    print("Block max width:", BlockWidth)
+    BlockWidthMax = min(Mark["Width"], Mark["Height"])
+    print("Block max width:", BlockWidthMax)
+
+    BlocksInMark = {}
+
+    for BlockWidth in range(BlockWidthMax, 0, -1):
+        print("Search, BlockWidth:", BlockWidth)
+
+        for X, Y in Mark["Coords"]:
+
+            CornerX = X+BlockWidth -1
+            CornerY = Y+BlockWidth -1
+
+            # if we try to leave the original dimensions:
+            if CornerX > Mark["Width"] - 1: continue
+            if CornerY > Mark["Height"] - 1: continue
+
+            print("analyse: ", X, Y, " -> ", CornerX, CornerY)
+
 
