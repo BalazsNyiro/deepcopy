a650ed7bcf61ea1bff7474e3c8ac24a9bc0b33fc
Balazs Nyiro <diogenesz@pergamen.hu>
2020-01-31 06:51:50 +0100

    test: block search in Mark

--- src/test_vector.py
+++ src/test_vector.py
@@ -8,8 +8,13 @@ class VectorTests(unittest.TestCase):
                "XXXX."
                "XXX..")
         MarkGenerated = mark_util.mark_from_string(Txt, 5, "X")
-        vector.block_search_in_mark(MarkGenerated)
-        self.assertEqual(1, 2)
+        BlocksInMark = vector.block_search_in_mark(MarkGenerated)
+
+        BlocksWanted = {3: [(0, 1)],
+                        2: [(2, 0), (3, 0), (0, 1), (1, 1), (2, 1), (0, 2), (1, 2)],
+                        1: [(2, 0), (3, 0), (4, 0), (0, 1), (1, 1), (2, 1), (3, 1),
+                            (4, 1), (0, 2), (1, 2), (2, 2), (3, 2), (0, 3), (1, 3), (2, 3)]}
+        self.assertEqual(BlocksInMark, BlocksWanted)
 
 def run_all_tests(P):
     print("run all tests: Vector")
--- src/vector.py
+++ src/vector.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
+# return: block exists, PixelWithProblem(Coords or None)
+# TODO: WRITE SEPARATED TEST for positive and negative cases
+def block_check_exist_in_mark(Mark, XTopLeft, YTopLeft, XBottomRight, YBottomRight):
+    for XMaybe in range(XTopLeft, XBottomRight + 1):
+        for YMaybe in range(YTopLeft, YBottomRight + 1):
+            if (XMaybe, YMaybe) not in Mark["Coords"]:
+                return False, (XMaybe, YMaybe)
+    return (True, None)
 
+# TESTED directly
 def block_search_in_mark(Mark):
     BlockWidthMax = min(Mark["Width"], Mark["Height"])
     print("Block max width:", BlockWidthMax)
@@ -7,17 +16,30 @@ def block_search_in_mark(Mark):
     BlocksInMark = {}
 
     for BlockWidth in range(BlockWidthMax, 0, -1):
-        print("Search, BlockWidth:", BlockWidth)
+        # print("Search, BlockWidth:", BlockWidth)
 
         for X, Y in Mark["Coords"]:
 
-            CornerX = X+BlockWidth -1
-            CornerY = Y+BlockWidth -1
+            XBottomRight = X+BlockWidth -1
+            YBottomRight = Y+BlockWidth -1
 
             # if we try to leave the original dimensions:
-            if CornerX > Mark["Width"] - 1: continue
-            if CornerY > Mark["Height"] - 1: continue
+            if XBottomRight > Mark["Width"] - 1: continue
+            if YBottomRight > Mark["Height"] - 1: continue
 
-            print("analyse: ", X, Y, " -> ", CornerX, CornerY)
+            BlockCheckExist, FirstProblem = block_check_exist_in_mark(Mark, X, Y, XBottomRight, YBottomRight)
+            ProblemReport = ""
+            if FirstProblem is not None:
+                ProblemReport = " first problem: " + str(FirstProblem)
+            else:
+                if BlockWidth not in BlocksInMark:
+                    BlocksInMark[BlockWidth] = []
+                BlocksInMark[BlockWidth].append((X,Y))
 
+            # print("analyse: ", X, Y, " -> ", XBottomRight, YBottomRight, BlockCheckExist, ProblemReport)
 
+    print("Blocks in Mark: ")
+    for CoordStart, BlockList in BlocksInMark.items():
+        print(CoordStart, BlockList)
+
+    return BlocksInMark
