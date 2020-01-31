cdc21a085d58cfaa53e2f38b6c54736a36cf195c
Balazs Nyiro <diogenesz@pergamen.hu>
2020-01-31 19:06:27 +0100

    test: non-overlapping block search

--- src/test_vector.py
+++ src/test_vector.py
@@ -2,18 +2,31 @@
 import unittest, vector, mark_util
 
 class VectorTests(unittest.TestCase):
-    def test_block_search_in_mark(self):
+    def test_block_check_exist_in_mark(self):
         Txt = ("..XXX"
                "XXXXX"
                "XXXX."
                "XXX..")
         MarkGenerated = mark_util.mark_from_string(Txt, 5, "X")
-        BlocksInMark = vector.block_search_in_mark(MarkGenerated)
+        BlockCheck, PixelProblem, CoordsOfBlock = vector.block_check_exist_in_mark(MarkGenerated, 0, 1, 2, 3)
+        self.assertEqual(BlockCheck, True)
+
+        BlockCheck, PixelProblem, CoordsOfBlock = vector.block_check_exist_in_mark(MarkGenerated, 0, 1, 4, 3)
+        self.assertEqual(BlockCheck, False)
+
+    def test_block_nonoverlap_search_in_mark(self):
+        Txt = ("..XXX"
+               "XXXXX"
+               "XXXX."
+               "XXX..")
+        MarkGenerated = mark_util.mark_from_string(Txt, 5, "X")
+        BlocksInMark = vector.block_nonoverlap_search_in_mark(MarkGenerated)
 
         BlocksWanted = {3: [(0, 1)],
-                        2: [(2, 0), (3, 0), (0, 1), (1, 1), (2, 1), (0, 2), (1, 2)],
-                        1: [(2, 0), (3, 0), (4, 0), (0, 1), (1, 1), (2, 1), (3, 1),
-                            (4, 1), (0, 2), (1, 2), (2, 2), (3, 2), (0, 3), (1, 3), (2, 3)]}
+                        2: [(2, 0), (0, 1)],
+                        1: [(2, 0), (3, 0), (4, 0), (0, 1), (1, 1),
+                            (2, 1), (3, 1), (4, 1), (0, 2), (1, 2),
+                            (2, 2), (3, 2), (0, 3), (1, 3), (2, 3)]}
         self.assertEqual(BlocksInMark, BlocksWanted)
 
 def run_all_tests(P):
--- src/vector.py
+++ src/vector.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 # return: block exists, PixelWithProblem(Coords or None)
-# TODO: WRITE SEPARATED TEST for positive and negative cases
+# TESTED
 def block_check_exist_in_mark(Mark, XTopLeft, YTopLeft, XBottomRight, YBottomRight):
+    CoordsOfBlock = []
     for XMaybe in range(XTopLeft, XBottomRight + 1):
         for YMaybe in range(YTopLeft, YBottomRight + 1):
             if (XMaybe, YMaybe) not in Mark["Coords"]:
-                return False, (XMaybe, YMaybe)
-    return (True, None)
+                return (False, (XMaybe, YMaybe), [])
+                # Block doesnt exist, PixelWithProblem
+            CoordsOfBlock.append((XMaybe, YMaybe))
+
+    return (True, None, CoordsOfBlock) # Block exists, No Problem
 
 # TESTED directly
-def block_search_in_mark(Mark):
+def block_nonoverlap_search_in_mark(Mark):
     BlockWidthMax = min(Mark["Width"], Mark["Height"])
     print("Block max width:", BlockWidthMax)
 
@@ -18,27 +22,39 @@ def block_search_in_mark(Mark):
     for BlockWidth in range(BlockWidthMax, 0, -1):
         # print("Search, BlockWidth:", BlockWidth)
 
-        for X, Y in Mark["Coords"]:
-
-            XBottomRight = X+BlockWidth -1
-            YBottomRight = Y+BlockWidth -1
+        # duplication of keys, I will remove the
+        # detected points so at the end Coords will be empty
+        Coords = dict(Mark["Coords"])
+        while Coords:
+            X, Y = list(Coords.keys())[0]
+            del Coords[(X, Y)]
 
             # if we try to leave the original dimensions:
-            if XBottomRight > Mark["Width"] - 1: continue
+            XBottomRight = X+BlockWidth -1;
+            if XBottomRight > Mark["Width"] - 1:  continue
+            YBottomRight = Y+BlockWidth -1;
             if YBottomRight > Mark["Height"] - 1: continue
 
-            BlockCheckExist, FirstProblem = block_check_exist_in_mark(Mark, X, Y, XBottomRight, YBottomRight)
-            ProblemReport = ""
+            BlockCheckExist, FirstProblem, CoordsOfBlock = block_check_exist_in_mark(Mark, X, Y, XBottomRight, YBottomRight)
+            # ProblemReport = ""
             if FirstProblem is not None:
-                ProblemReport = " first problem: " + str(FirstProblem)
+                # ProblemReport = " first problem: " + str(FirstProblem)
+                pass
             else:
                 if BlockWidth not in BlocksInMark:
                     BlocksInMark[BlockWidth] = []
                 BlocksInMark[BlockWidth].append((X,Y))
 
+                # remove founded area from the set
+                # to avoid overlapping areas
+                for CoordInBlock in CoordsOfBlock:
+                    if CoordInBlock in Coords: # we delete the top left pixel after the while immediately
+                                               # so one pixel maybe not in the dict
+                        del Coords[CoordInBlock]
+
             # print("analyse: ", X, Y, " -> ", XBottomRight, YBottomRight, BlockCheckExist, ProblemReport)
 
-    print("Blocks in Mark: ")
+    print("Non-overlap Blocks in Mark: ")
     for CoordStart, BlockList in BlocksInMark.items():
         print(CoordStart, BlockList)
 
