6ba7a06686d4d58d6672b7f08a8135e1c63f7f71
Balazs Nyiro <diogenesz@pergamen.hu>
2020-02-02 11:46:46 +0100

    Spiral usage instead of blocks, initial steps

--- src/test_vector.py
+++ src/test_vector.py
@@ -2,31 +2,17 @@
 import unittest, vector, mark_util
 
 class VectorTests(unittest.TestCase):
-    def test_block_check_exist_in_mark(self):
-        Txt = ("..XXX"
-               "XXXXX"
-               "XXXX."
-               "XXX..")
-        MarkGenerated = mark_util.mark_from_string(Txt, 5, "X")
-        BlockCheck, PixelProblem, CoordsOfBlock = vector.block_exist_in_coords(MarkGenerated["Coords"], 0, 1, 2, 3)
-        self.assertEqual(BlockCheck, True)
+    def test_spiral_max_from_coord(self):
+        Letter = ("OOOO"
+                  "OOOO"
+                  " OOO"
+                  "OOOO")
+        Mark = mark_util.mark_from_string(Letter, 4, "O")
+        print("\n##############################")
+        SpiralMax = vector.spiral_max_from_coord(Mark, (2,1))
+        print("SpiralMax:", SpiralMax)
 
-        BlockCheck, PixelProblem, CoordsOfBlock = vector.block_exist_in_coords(MarkGenerated["Coords"], 0, 1, 4, 3)
-        self.assertEqual(BlockCheck, False)
-
-    def test_block_nonoverlap_search_in_mark(self):
-        Txt = ("..XXX"
-               "XXXXX"
-               "XXXX."
-               "XXX..")
-        MarkGenerated = mark_util.mark_from_string(Txt, 5, "X")
-        BlocksInMark = vector.block_nonoverlap_search_in_mark(MarkGenerated)
-        BlocksWanted = {3: [[(0, 1), (0, 2), (0, 3), (1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3)]],
-                        2: [[(3, 0), (3, 1), (4, 0), (4, 1)]],
-                        1: [[(2, 0)], [(3, 2)]]}
-        self.assertEqual(BlocksInMark, BlocksWanted)
-
-    def test_block_nonoverlap_search_in_mark__letter_e(self):
+    def test_spiral_nonoverlap_search_in_mark__letter_e(self):
         Letter = ("    OOOOOOO   "
                   "  OOOOOOOOOO  "
                   " OOOOOOOOOOOO "
@@ -43,8 +29,8 @@ class VectorTests(unittest.TestCase):
                   "  OOOOOOOOOOO "
                   "    OOOOOOOO  ")
         MarkGenerated = mark_util.mark_from_string(Letter, 14, "O")
-        BlocksInMark = vector.block_nonoverlap_search_in_mark(MarkGenerated)
-        print("\n" + vector.block_to_string(MarkGenerated, BlocksInMark, Prefix=" ") + "\n")
+        #BlocksInMark = vector.block_nonoverlap_search_in_mark(MarkGenerated)
+        #print("\n" + vector.block_to_string(MarkGenerated, BlocksInMark, Prefix=" ") + "\n")
         # TODO: display the blocks with colors and finish the test
 
 def run_all_tests(P):
--- src/vector.py
+++ src/vector.py
@@ -1,68 +1,55 @@
 # -*- coding: utf-8 -*-
-import mark_util, area
+import copy
 
-# return: block exists, PixelWithProblem(Coords or None)
-# TESTED
-def block_exist_in_coords(Coords, XTopLeft, YTopLeft, XBottomRight, YBottomRight):
-    CoordsOfBlock = []
-    for XMaybe in range(XTopLeft, XBottomRight + 1):
-        for YMaybe in range(YTopLeft, YBottomRight + 1):
-            if (XMaybe, YMaybe) not in Coords:
-                return (False, (XMaybe, YMaybe), [])
-                # Block doesnt exist, PixelWithProblem
-            CoordsOfBlock.append((XMaybe, YMaybe))
+# start_down: x modify, y modify,  step 2 xmodify, ymodify...
+_Up = (0, -1)
+_Down = (0, 1)
+_Left = (-1, 0)
+_Right = (1, 0)
+_SpiralOperators = {
+    "StartDown" : [_Down, _Right, _Up, _Left],
+    "StartRight": [_Right, _Up, _Left, _Down],
+    "StartUp"   : [_Up, _Left, _Down, _Right],
+    "StartLeft" : [_Left, _Down, _Right, _Up]
+}
 
-    return (True, None, CoordsOfBlock) # Block exists, No Problem
+def spiral_max_from_coord(Mark, Coord):
+    DirectionOperators = copy.deepcopy(_SpiralOperators["StartDown"])
+    SpiralCoords = []
+    SpiralCoords.append(Coord)
+    return SpiralCoords
 
-# TESTED directly
-def block_nonoverlap_search_in_mark(Mark):
-    BlockWidthMax = min(Mark["Width"], Mark["Height"])
-    print("Block max width:", BlockWidthMax)
+# Spiral search from point 1:
+#                  5   56  567  567  567  567  567  567   567   567   567  g567
+#    1  1   1  41  41  41  41   418  418  418  418  418   418   418  f418  f418
+#       2  32  32  32  32  32   32   329  329  329  329   329  e329  e329  e329
+#                                           a   ba  cba  dcba  dcba  dcba  dcba
 
-    BlocksInMark = {}
+def spiral_nonoverlap_search_in_mark(Mark):
+    SpiralsInMark = {}
     CoordsTry = dict(Mark["Coords"])
 
-    def coordinates_possible_block(Coord, BlockWidth):
-        XTopLeft, YTopLeft = Coord
-        XBottomRight = XTopLeft + BlockWidth - 1
-        YBottomRight = YTopLeft + BlockWidth - 1
-        return XTopLeft, YTopLeft, XBottomRight, YBottomRight
+    while CoordsTry:
 
-    def coords_delete(Coords, CoordsDeleted):
-        for CoordDel in CoordsDeleted:
-            print("  del:", CoordDel)
-            del Coords[CoordDel]
+        SpiralBiggestCoordStart = (-1, -1)
+        SpiralBiggestCoords = [] # the order of coords are important to represent the spiral
 
-    def blocks_in_mark_save(BlockWidth, BlocksInMark, CoordsOfBlock):
-        if BlockWidth not in BlocksInMark:
-            BlocksInMark[BlockWidth] = []
-        BlocksInMark[BlockWidth].append(CoordsOfBlock)
+        for Coord in CoordsTry:
+            SpiralMaxNow = sprial_max_from_coord(Coord)
+            if len(SpiralMaxNow) > len(SpiralBiggestCoords):
+                SpiralBiggestCoords = SpiralMaxNow
+                SpiralBiggestCoordStart = Coord
 
-    for BlockWidth in range(BlockWidthMax, 0, -1):
-        print("\nBlockWidth: ", BlockWidth) # 4, 3, 2, 1
-
-        # CoordsTry has to be a dict because we delete elements during the cycle
-        for Coord in dict(CoordsTry).keys(): # duplicate Coords because it changes in the cycle
-            if Coord not in CoordsTry: continue # runtime we delete from Coords
-
-            XTopLeft, YTopLeft, XBottomRight, YBottomRight = coordinates_possible_block(Coord, BlockWidth)
-            print(" Coord >", Coord, XTopLeft, YTopLeft, XBottomRight, YBottomRight )
-
-            BlockExists, _FirstProblem, CoordsOfBlock = \
-                block_exist_in_coords(CoordsTry, XTopLeft, YTopLeft, XBottomRight, YBottomRight)
-
-            if BlockExists:
-                print("  CoordsOfBlock", CoordsOfBlock)
-                coords_delete(CoordsTry, CoordsOfBlock)
-                blocks_in_mark_save(BlockWidth, BlocksInMark, CoordsOfBlock)
+        SpiralsInMark[SpiralBiggestCoordStart] = SpiralBiggestCoords
+        coords_delete(CoordsTry, SpiralBiggestCoords)
 
 
-    print("Non-overlap Blocks in Mark: ")
-    for CoordStart, BlockList in BlocksInMark.items():
-        print(CoordStart, BlockList)
+    return SpiralsInMark
 
-    return BlocksInMark
-
+def coords_delete(CoordsDict, CoordsDeletedList):
+    for CoordDel in CoordsDeletedList:
+        print("  del:", CoordDel)
+        del CoordsDict[CoordDel]
 
 # these chars have colors in Linux terminal, I hope in windows there are colored chars, too
 # https://apps.timwhitlock.info/emoji/tables/unicode
@@ -78,16 +65,4 @@ CharsetColorful = [
     "🔆", # high brightness symbol
     "🔘", #radio button,
 ]
-def block_to_string(Mark, BlocksInMark, CharSet=CharsetColorful, CharBg=CharBg, Prefix=""):
-    # print("Chars:")
-    #for C in CharSet:
-    #    print(C, C, C)
-
-    Area = mark_util.mark_to_area(Mark)
-    for BlockSize, CoordsInBlock in BlocksInMark.items():
-        print("Coords in block: ", CoordsInBlock)
-        for CoordBlock in CoordsInBlock:
-            for X, Y in CoordBlock:
-                Area[X][Y] = CharSet[BlockSize]
-    return area.to_string(Area, Prefix=Prefix).replace(".", CharBg)
 
