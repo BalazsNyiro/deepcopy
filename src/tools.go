1f9a5272d57e0e882e93faf85dca430c20743d2e
Balazs Nyiro <diogenesz@pergamen.hu>
2021-09-06 21:37:40 -0400

    basic funs into tools

---
+++ src/tools.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"log"
+	"os"
+)
+
+type TypeHistogram [16][4]int
+
+func img_read_from_file(FileName string) image.Image {
+	reader, err := os.Open(FileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ImageFromFile, _, err := image.Decode(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	reader.Close()
+	return ImageFromFile
+}
+
+func histogram_create(Img image.Image) TypeHistogram {
+	bounds := Img.Bounds()
+	// based on: https://pkg.go.dev/image@go1.17  (respect)
+
+	// Calculate a 16-bin Histogram for m's red, green, blue and alpha components.
+	//
+	// An image's bounds do not necessarily start at (0, 0), so the two loops start
+	// at bounds.Min.Y and bounds.Min.X. Looping over Y first and X second is more
+	// likely to result in better memory access patterns than X first and Y second.
+	var Histogram TypeHistogram
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, a := Img.At(x, y).RGBA()
+			// A color's RGBA method returns values in the range [0, 65535].
+			// Shifting by 12 reduces this to the range [0, 15].
+			Histogram[r>>12][0]++
+			Histogram[g>>12][1]++
+			Histogram[b>>12][2]++
+			Histogram[a>>12][3]++
+		}
+	}
+	return Histogram
+}
+
+func histogram_result_print(Histogram TypeHistogram) {
+	fmt.Printf("%-14s %6s %6s %6s %6s\n", "bin", "red", "green", "blue", "alpha")
+	for i, x := range Histogram {
+		fmt.Printf("0x%04x-0x%04x: %6d %6d %6d %6d\n", i<<12, (i+1)<<12-1, x[0], x[1], x[2], x[3])
+	}
+}
