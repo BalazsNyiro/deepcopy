4ca5a03a5b4dcbbd305585e1d4bf616f1cb1e765
Balazs Nyiro <diogenesz@pergamen.hu>
2022-05-02 22:38:04 -0400

    image_generator

--- test/generate_test_images.sh
+++ test/generate_test_images.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env bash
 # fontlist: fc-list
 # convert -list fonts
-
-TRUETYPE_FONT='CourierNew'
-TRUETYPE_FONT='Z003-MediumItalic'
-
 # https://legacy.imagemagick.org/Usage/text/
 function render () {
   FONT_USED="$2" # abs path of ttf file
   echo $FONT_USED
-  # WORK
-  # gm convert -font 'Helvetica'   -fill green -draw 'text 50,300 Magick' -pointsize 100  ~/flora-hearts.jpg annotated.gif
-  gm convert -font 'l-regular-i-normal--0-0-0-0-p-0-iso8859-2'   -fill green -draw 'text 50,300 Magicktext' -pointsize 100  ~/flora-hearts.jpg annotated.gif
-
-  # list all possible argument
-  # convert -list font
-  convert -font "$2"   -pointsize 36 label:"$1" -gravity center test.png 
-  display test.png
-
+  PNG_FILE="test_${2}.png"
+  convert -font "$2"   -pointsize 36 label:"$1" -gravity center "$PNG_FILE" 
+  display "$PNG_FILE"
 }
-render "Árvíztűrő tükörfúrógép 0123456789+-_?.!=" $TRUETYPE_FONT
+# list all possible argument
+# convert -list font
+TXT="Árvíztűrő tükörfúrógép 0123456789+-_?.!=" 
+render "$TXT" "Z003-MediumItalic"
+render "$TXT" "Waree-Bold-Oblique"
