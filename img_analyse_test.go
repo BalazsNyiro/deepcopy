1df9a2a02f714f9506fabee4d3314db782a4a4a0
Balazs Nyiro <diogenesz@pergamen.hu>
2022-06-16 22:08:34 -0400

    test fix

--- img_analyse_test.go
+++ img_analyse_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func linking_test(x, y int, neighbour string, targetX, targetY int, pixelMap PixelMap, t *testing.T) {
+func linking_test(testId string, x, y int, neighbour string, targetX, targetY int, pixelMap PixelMap, t *testing.T) {
 	neighbourId := pixelMap[x][y].n1.id
 	if neighbour == "n2" { neighbourId = pixelMap[x][y].n2.id }
 	if neighbour == "n3" { neighbourId = pixelMap[x][y].n3.id }
@@ -16,7 +16,8 @@ func linking_test(x, y int, neighbour string, targetX, targetY int, pixelMap Pix
 	if neighbour == "n8" { neighbourId = pixelMap[x][y].n8.id }
 
 	if neighbourId != pixelMap[targetX][targetY].id {
-		t.Fatalf("linking problem! %d,%d.%s.id = %d 0,1.id = %d", x, y, neighbour, neighbourId, pixelMap[targetX][targetY].id)
+		t.Fatalf("TestId %s - linking problem! %d,%d.%s.id = %d 0,1.id = %d",
+			testId, x, y, neighbour, neighbourId, pixelMap[targetX][targetY].id)
 	}
 
 }
@@ -31,10 +32,10 @@ func Test_pixel_neighbours_linking(t *testing.T) {
 	pixel_group_link_pixels(0, 0, &page)
 	print_pixel_map(pixelMap, "debug")
 
-	linking_test(0, 0, "n4", 1, 1, pixelMap, t)
-	linking_test(0, 0, "n5", 0, 1, pixelMap, t)
+	linking_test("neighbour_link_0_0 a", 0, 0, "n4", 1, 1, pixelMap, t)
+	linking_test("neighbour_link_0_0 b", 0, 0, "n5", 0, 1, pixelMap, t)
 
-	linking_test(0, 1, "n5", 0, 2, pixelMap, t)
+	linking_test("neighbour_link_0_1", 0, 1, "n5", 0, 2, pixelMap, t)
 
 
 
