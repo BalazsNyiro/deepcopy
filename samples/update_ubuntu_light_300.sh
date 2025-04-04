0b1a18fee67aaa7593ff9375ce915d94b034a97c
Balazs Nyiro <diogenesz@pergamen.hu>
2025-04-04 17:58:53 -0400

    typo, rename

--- samples/update_ubuntu_light_300.sh
+++ samples/update_ubuntu_light_300.sh
@@ -5,6 +5,6 @@ URL="https://fonts.google.com/specimen/Ubuntu?preview.text=$TEXT"
 echo "URL: $URL"
 google-chrome --headless --disable-gpu  --screenshot --window-size=2000,2000  --virtual-time-budget=15000  "$URL"
 
-gm convert screenshot.png -crop 1000x90+300+950 sample_ubuntu_light_300.png
+gm convert screenshot.png -crop 1000x90+300+950 sample_abc_lower_ubuntu_light_300.png
 
 rm screenshot.png
