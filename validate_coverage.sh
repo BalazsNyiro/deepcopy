f4d5f3cb9698d6deba8be9f299cbe2d9b5907f4f
Balazs Nyiro <diogenesz@pergamen.hu>
2025-04-06 20:10:46 -0400

    coverage

--- validate_coverage.sh
+++ validate_coverage.sh
@@ -5,8 +5,13 @@ for DIR_NAME in src/*; do
     if test -d $DIR_NAME; then
         echo "=== Dir, to check cover with tests: $DIR_NAME ==="
         cd $DIR_NAME
-        python3 -m coverage run -m unittest
+        pwd
+
+        rm .coverage
+
+        python3 -m coverage run -m unittest *test.py
         python3 -m coverage report
+
         cd $DIR_START
     fi
 done
