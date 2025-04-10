1edb57d17ea32105c6df8bac0590e8ca6e516077
Balazs Nyiro <diogenesz@pergamen.hu>
2025-04-10 03:35:28 -0400

    licence into all py files

--- src/1_utils/img_pixels_test.py
+++ src/1_utils/img_pixels_test.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2025 Balazs Nyiro
+# All rights reserved.
+
+# This source code (all file in this repo)
+# is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Please read the complete LICENSE file
+# in the root directory of this source tree.
+
+
+
 # python3 img_pixels_test.py
 # python3 img_pixels_test.py  Test_active_pixel_group_detection.test_active_pixel_group_detection
 
