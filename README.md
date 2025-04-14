2c65cf5ad126525e175bd168840707b94354e275
Balazs Nyiro <diogenesz@pergamen.hu>
2025-04-14 06:45:53 -0400

    install support

--- README.md
+++ README.md
@@ -6,12 +6,16 @@ Contact/Author: Balazs Nyiro, balazs.nyiro.ca@gmail.com
 How can you support:
  - I need a Macintosh to test Deepcopy, so if you have an unused and can give that to me, that would be a help.
 
+# INSTALL support
+deepcopy_installed_correctly.py -> check available modules and print install hints.
+
 # INSTALL
  - important python3 modules
    - python3-pil  
      - apt install python3-pil    (install with apt package manager)
      - pip install pillow         (install with pip)
-
+   - multiprocessing (works on Linux/win)
+   - multiprocess   (only Mac)    (pip install multiprocess)  pip install multiprocess
  
 # INSTALL for development
 
---
+++ deepcopy_installed_correctly.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2025 Balazs Nyiro
+# All rights reserved.
+
+# This source code (all file in this repo)
+# is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Please read the complete LICENSE file
+# in the root directory of this source tree.
+
+
+
+
+import os, sys
+print("This script checks the available modules, to see if deepcopy can work or not")
+
+try:
+    print("pillow (PIL) is important to read images and create screenshots")
+    from PIL import Image
+    print(f"PIL import was successful")
+
+except:
+    print("""
+          Python image library (PIL) is important to load images.
+          PIL import was unsuccessful - please install pillow: 'pip install pillow'
+          """)
+
+print(f"multiprocessing is useful to speed up image recognition - deepcopy can work without this, but will be slow.")
+print(f"multiprocessing is working on Linux/Win only, multiprocess works on Mac - so please install any of them to speed up the program")
+
+try:
+    print(f"multiprocessing on Linux/Win... (https://stackoverflow.com/questions/61270799/python-multiprocessing-with-macos)")
+    import multiprocessing as mp
+    print(f"multiprocessing import was successful")
+
+except:
+    print(f"Module multiprocessing is Not available")
+    print(f"try to import multiprocess... (https://stackoverflow.com/questions/61270799/python-multiprocessing-with-macos)")
+
+    try:
+        import multiprocess as mp
+    except:
+        print(f"Module multiprocess is Not available")
+
+        print("Theoretically deepcopy can work without multiprocessing/multiprocess but radically slower")
+        print("Practically we will see this part - if you have problems, install multiprocess.")
+
