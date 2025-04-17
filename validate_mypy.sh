42b2ae282523a9fbcf5404d3bea0d479e05aa896
Balazs Nyiro <diogenesz@pergamen.hu>
2025-04-17 13:53:07 -0400

    pyproject

--- validate_mypy.sh
+++ validate_mypy.sh
@@ -7,15 +7,12 @@ else
   # mypy src/p0_base/prg_general_config_and_state.py
   # mypy src/p1_pixels/img_0_pixels.py
 
+
   for FILE_PY in *.py  */*/*.py; do
-
-    if [[ "$FILE_PY" != *"_test.py" ]]; then
-
-      echo "===== normal py file, mypy type validation: $FILE_PY ====="
-      mypy $FILE_PY
-
-    fi
-
+    echo
+    echo $FILE_PY
+    mypy $FILE_PY
   done
+
 fi
 
